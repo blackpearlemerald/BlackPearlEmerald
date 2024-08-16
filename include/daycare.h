8a365244ab44dd429fa3fb4415971f27384214f1
CD <cddell135@gmail.com>
2024-08-16 12:52:54 -0500

    Fixing bugs with egg/daycare

--- include/daycare.h
+++ include/daycare.h
@@ -32,14 +32,13 @@ void SetDaycareCompatibilityString(void);
 bool8 NameHasGenderSymbol(const u8 *name, u8 genderRatio);
 void ShowDaycareLevelMenu(void);
 void ChooseSendDaycareMon(void);
-//UPDATE TO 1.8.6 CHANGED THIS BELOW
+
 u16 GetEggSpecies(u16 species);
 u16 GetEggMovesArraySize(void);
 // to support gEggMoves being referenced by pokemon.c
 extern const u16 gEggMoves[];
 
-u8 GetEggMovesBySpecies(u16 species, u16 *eggMoves);
-// CHANGED THIS ABOVE
+u8 GetEggMovesSpecies(u16 species, u16 *eggMoves);
 bool8 SpeciesCanLearnEggMove(u16 species, u16 move);
 
 #endif // GUARD_DAYCARE_H
--- src/pokedex_plus_hgss.c
+++ src/pokedex_plus_hgss.c
@@ -5130,7 +5130,8 @@ static bool8 CalculateMoves(void)
         species = GetFormSpeciesId(species, 0);
 
     //Calculate amount of Egg and LevelUp moves
-    numEggMoves = GetEggMovesBySpecies(species, statsMovesEgg);
+    //numEggMoves = GetEggMovesBySpecies(species, statsMovesEgg);
+    numEggMoves = GetEggMovesSpecies(species, statsMovesEgg);
     numLevelUpMoves = GetLevelUpMovesBySpecies(species, statsMovesLevelUp);
 
     //Egg moves
